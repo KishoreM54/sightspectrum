b173c348b17c230f3921bdc9559e55931565c40a
KishoreM54 <kishorem.sightspectrum@Gmail.com>
2023-06-06 04:18:50 -0700

    first commit

---
+++ week2_task2/dict methods - Copy.py
@@ -0,0 +1,12 @@
+##------------------------dictionary methods------------
+##------key()------
+##x={"name":"mani","age":26}
+##x.items()
+##print(x)
+##print(x.items())
+##x={"name":"mani","age":26}
+##print(x.keys())
+##x={"name":"mani","age":26}
+##print(x.values())
+##----------access---------
+##get()
