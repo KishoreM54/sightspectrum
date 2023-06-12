187350a0421d05e1f6bef5ad6fa0853f29ffadaa
KishoreM54 <kishorem.sightspectrum@Gmail.com>
2023-06-12 03:26:17 -0700

    first commit

---
+++ week3_task1/week 2 friday.py
@@ -0,0 +1,220 @@
+##x=3+2
+##print(x)
+
+##x=3**4.9
+##print(x)
+
+##a=8
+##b=a==10
+##print(b)
+##a=8
+##b=a!=10
+##print(b)
+##
+####AND gate=true true only true OR gate opposite
+
+##if amount>500:
+##    discount=10% else 0%
+##amount=6000
+##discount=0.1*amount if amount>500 else 0
+##print(discount)
+
+
+##rate=1000
+##discount=0.1*rate if rate>920 else 0
+##print(discount)
+
+##------if------------(answer not found)
+##
+##a = 1000
+##b = 800
+##
+##if 5 < 10 :
+##    print("5 is greater than 10")
+##    print("c")
+##elif a==b:
+##    c="a is equal to b"
+##    print(c)
+##--------------------condition is true ,output will came otherwise dont
+##if 10>5:
+##    print("true")
+##    print("dance")
+
+##if 10!=9:
+##    print("true")
+
+
+
+##if 10>5 and 3<5:
+##    print("true")
+##    print("dance")
+##
+##if 10>5 or 3>5:
+##    print("true")
+##    print("dance")
+##x=10
+##if x==8:----------------(%*/+_/)
+##    print("true")
+##    print("dance")
+##else:
+##    print("party")
+
+##if x==8:
+##    print("true")
+##    print("dance")
+##elif x<=10:
+##    print("vibe")
+##else:
+##    print("party")
+##n=10
+##def func():
+##
+##    if n==6:
+##        print(n)
+##    else:
+##            print(n*fact(n-1))
+
+
+#----------for loop------------
+
+##for i in range(0,10):
+##    print(i)
+
+##for i in range(0,10,3):
+##    print(i)
+
+
+##x=9
+##while x<20:
+##    print(x)
+##    x += 1
+
+##i=3
+##a=6
+##for(i= 1,i<=5):
+##    if(a[i]>max)
+##    max = a[i]
+##    print("max")
+##
+##a=10
+##if a>=12:
+##    print("akon")
+##elif a==10:
+##    print("bat")
+##else:
+##    print("justin")
+##
+##rate=400
+##discount=0.2*rate if rate>500 else 100
+##print(discount)
+
+##name=input("enter your name:")
+##while name =="":
+##
+##amount=500
+##discount=0.3*amount if amount>450 else 0
+##print(discount)
+
+##
+##age=int(input("enter gayu age:"))
+##while age>4:
+##    print("age can be negative")
+##    age=int(input("enter gayu age"))
+##print(f"gayu are {age} years old")
+
+##
+##name=input("enter gayu name:")
+##while name =="":
+##    print("forgot to enter")
+##    name=input("enter gayu name:")
+##    print("hello{daisy}")
+##print("hello gayu")
+
+##i=4
+##while i<=9:
+##    print(i)
+##    i+=1
+##else:
+##    print("dance with kishore")
+##fruits=["apple","banana","cherry"]
+##for i in fruits:
+##    print(i)
+##    if i == "banana":
+##       break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
